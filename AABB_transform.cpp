25af828c711e0c53b1a832ac3bb96188a1a4cf38
Aslan Dzodzikov <AslanD@list.ru>
2023-07-29 02:47:03 +0300

    Slight test polish (cosmetics)

--- AABB_transform.cpp
+++ AABB_transform.cpp
@@ -14,7 +14,6 @@ using glm::mat3x4;
 
 inline std::array<vec3, 8> TransformBBPoints(const mat3x4& transform, const vec3& maxPoint, const vec3& minPoint)
 {
-
     const vec3 size = maxPoint - minPoint;
 
     auto basisVector = [&](int i)
@@ -34,32 +33,23 @@ inline std::array<vec3, 8> TransformBBPoints(const mat3x4& transform, const vec3
              out[0] + basis[2], out[1] + basis[2], out[2] + basis[2], out[3] + basis[2] };
 }
 
-void GetBBPoints(vec3* outPoints, const vec3& maxPoint, const vec3& minPoint)
+std::array<vec3, 8> GetBBPoints(const vec3& maxPoint, const vec3& minPoint)
 {
-    assert(outPoints && "NULL Pointer");
-
-    outPoints[0] = minPoint;
-    outPoints[1] = vec3(maxPoint.x, minPoint.y, minPoint.z);
-    outPoints[2] = vec3(minPoint.x, maxPoint.y, minPoint.z);
-    outPoints[3] = vec3(maxPoint.x, maxPoint.y, minPoint.z);
-    outPoints[4] = vec3(minPoint.x, minPoint.y, maxPoint.z);
-    outPoints[5] = vec3(maxPoint.x, minPoint.y, maxPoint.z);
-    outPoints[6] = vec3(minPoint.x, maxPoint.y, maxPoint.z);
-    outPoints[7] = maxPoint;
+    return { minPoint,
+             vec3(maxPoint.x, minPoint.y, minPoint.z),
+             vec3(minPoint.x, maxPoint.y, minPoint.z),
+             vec3(maxPoint.x, maxPoint.y, minPoint.z),
+             vec3(minPoint.x, minPoint.y, maxPoint.z),
+             vec3(maxPoint.x, minPoint.y, maxPoint.z),
+             vec3(minPoint.x, maxPoint.y, maxPoint.z),
+             maxPoint
+    };
 }
 
 void test(const mat3x4& transform, const vec3& maxPoint, const vec3& minPoint)
 {
     auto outPoints = TransformBBPoints(transform, maxPoint, minPoint);
-    decltype(outPoints) testPoints;
-    decltype(outPoints) pts;
-
-    GetBBPoints(pts.data(), maxPoint, minPoint);
-
-    for (const vec3& pt : pts)
-    {
-        testPoints[&pt - pts.data()] = vec4(pt, 1.f) * transform;
-    }
+    auto pts = GetBBPoints(maxPoint, minPoint);
 
     auto epsEqual = [](float a, float b)
         {
@@ -69,9 +59,10 @@ void test(const mat3x4& transform, const vec3& maxPoint, const vec3& minPoint)
 
     for (size_t i = 0; i < 8; ++i)
     {
-        const auto& a = outPoints[i];
-        const auto& b = testPoints[i];
-        if (!(epsEqual(a.x, b.x) && epsEqual(a.y, b.y) && epsEqual(a.z, b.z)))
+        const auto& oPoint = outPoints[i];
+        const auto& tPoint = vec4(pts[i], 1.f) * transform;
+
+        if (!(epsEqual(oPoint.x, tPoint.x) && epsEqual(oPoint.y, tPoint.y) && epsEqual(oPoint.z, tPoint.z)))
         {
             std::cout << "Error detected!\n";
             abort();
